daabcb0ffce8a4183028200da7d0bc3617190fe5
pjangir1235 <piyush.jangir@accoliteindia.com>
2017-12-29 15:44:46 +0530

    Add Piyush Function where it print my name

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,14 @@ public class Hello {
 	void helloBunny() {
         System.out.println("Hello Bunny!");
     }
+	void helloPiyush() {
+        System.out.println("Hello Piyush!");
+    }
     public static void main( String[] args ) {
         Hello h = new Hello();
+        HelloPiyush he=new HelloPiyush();
         h.helloBunny();
+        h.helloPiyush();
+        he.piyush();
     }
 }
