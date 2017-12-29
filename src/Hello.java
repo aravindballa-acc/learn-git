02eed1643ef010c30bfe6e7f36b8cb2c4cc59399
sukeshkancharla8 <sukesh.kancharla@accoliteindia.com>
2017-12-29 16:14:56 +0530

    added sukesh file and added extra functionality  for details

--- src/Hello.java
+++ src/Hello.java
@@ -2,29 +2,25 @@ public class Hello {
 	void helloBunny() {
         System.out.println("Hello Bunny!");
     }
-<<<<<<< HEAD
+
 	void printSukeshDetails() {
 		System.out.println("Sukesh");
 		System.out.println("Accolite");
 		System.out.println("Hyderabad");
 	}
-=======
+
 	void Print_Lokesh() {
 		System.out.println("Hello Lokesh in Hello File!!");
 	}
 	
->>>>>>> 0b020f412ef2ae57bf7c839883944da91e9597c5
+
     public static void main( String[] args ) {
         Hello h = new Hello();
-        Lokesh l = new Lokesh();
         h.helloBunny();
-<<<<<<< HEAD
         h.printSukeshDetails();
         Sukesh sukesh=new Sukesh();
         sukesh.printDetails();
-=======
-        h.Print_Lokesh();
-        l.Print_Lokesh();
->>>>>>> 0b020f412ef2ae57bf7c839883944da91e9597c5
+
+        
     }
 }
