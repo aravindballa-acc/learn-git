10713d1710961d2b468cb0186ae3be7d18019d06
saitejamalapati <saiteja.malapati@accoliteindia.com>
2017-12-29 15:48:44 +0530

    Added a function in Hello.java to print my name and added a Saiteja.java file with a function to print my details.

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,15 @@ public class Hello {
 	void helloBunny() {
         System.out.println("Hello Bunny!");
     }
+	
+	void hellosaiteja(){
+		System.out.println("Hello Sai Teja Reddy\n");
+	}
+	
     public static void main( String[] args ) {
         Hello h = new Hello();
         h.helloBunny();
+        Saiteja s = new Saiteja();
+        s.helloSaiteja();
     }
 }
