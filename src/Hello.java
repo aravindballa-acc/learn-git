2d4e5564a18b5fb2d4e6f10c169c359b8fa34ed0
urstrulyraghu <karanamraghuvardhan12@gmail.com>
2017-12-29 15:49:29 +0530

    Added file of my details and function to print my name

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,15 @@ public class Hello {
 	void helloBunny() {
         System.out.println("Hello Bunny!");
     }
+	
+	public void printName() {
+		System.out.println("Print Raghu");
+	}
+	
     public static void main( String[] args ) {
         Hello h = new Hello();
         h.helloBunny();
+        Raghu r = new Raghu();
+        r.printName();
     }
 }
