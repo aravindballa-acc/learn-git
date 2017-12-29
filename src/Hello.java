3b95a6711bc3d053c170949ab41f87de0d81113d
rithvik-reddy-koripelli <rithvik.reddy@accoliteindia.com>
2017-12-29 15:47:40 +0530

    Added Rithvik.java and modified Hello.java to include a function that prints my name

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,13 @@ public class Hello {
 	void helloBunny() {
         System.out.println("Hello Bunny!");
     }
+    void helloRithvik() {
+        System.out.println("Hello Rithvik!");
+    }
+
     public static void main( String[] args ) {
         Hello h = new Hello();
         h.helloBunny();
+        h.helloRithvik();
     }
 }
