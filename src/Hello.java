0b020f412ef2ae57bf7c839883944da91e9597c5
LokeshDogga <Lokesh.Dogga@accoliteindia.com>
2017-12-29 15:45:51 +0530

    Add Lokesh.java File to print my name

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,15 @@ public class Hello {
 	void helloBunny() {
         System.out.println("Hello Bunny!");
     }
+	void Print_Lokesh() {
+		System.out.println("Hello Lokesh in Hello File!!");
+	}
+	
     public static void main( String[] args ) {
         Hello h = new Hello();
+        Lokesh l = new Lokesh();
         h.helloBunny();
+        h.Print_Lokesh();
+        l.Print_Lokesh();
     }
 }
